19aa6d4f75896b547eeef0546465a26c8b8873d6
Benjamin Keys <bkeys818@gmail.com>
2020-09-25 18:00:14 -0400

    Finish Project 13 - InstaFilter & Begin Proejct 14

--- Apps/Project 13 - InstaFilter/InstaFilter/ContentView.swift
+++ Apps/Project 13 - InstaFilter/InstaFilter/ContentView.swift
@@ -12,12 +12,19 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
+
+    
+    @State private var inputImage: UIImage?
+    @State private var proccesedImage: UIImage?
+    
+    @State var currentFilter: CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     @State private var shownigImagePicker = false
-    @State private var inputImage: UIImage?
-    
-    @State var currentFilter = CIFilter.sepiaTone()
-    let context = CIContext()
+    @State private var showingActionSheet = false
+    @State private var showingSavingErrorAlert = false
     
     var body: some View {
         let intensity = Binding<Double>(
@@ -26,6 +33,24 @@ struct ContentView: View {
             },
             set: {
                 self.filterIntensity = $0
+                self.applyProccessing()
+            }
+        )
+        let radius = Binding<Double>(
+            get: {
+                self.filterRadius
+            },
+            set: {
+                self.filterRadius = $0
+                self.applyProccessing()
+            }
+        )
+        let scale = Binding<Double>(
+            get: {
+                self.filterScale
+            },
+            set: {
+                self.filterScale = $0
                 self.applyProccessing()
             }
         )
@@ -53,16 +78,37 @@ struct ContentView: View {
                     Text("Intensity")
                     Slider(value: intensity)
                 }
+                HStack {
+                    Text("Radius")
+                    Slider(value: radius)
+                }
+                HStack {
+                    Text("Scale")
+                    Slider(value: scale)
+                }
                 .padding(.vertical)
                 HStack {
-                    Button("Change Filter") {
-                        // change filter
+                    Button(action: {
+                        showingActionSheet = true
+                        setFilter(currentFilter)
+                    }) {
+                        VStack {
+                            Text("Change Filter")
+                            Text("Current: \(currentFilter.name)")
+                                .font(.caption)
+                        }
                     }
                     
                     Spacer()
                     
                     Button("Save") {
-                        // save image
+                        guard let proccesedImage = self.proccesedImage else { return }
+                        
+                        let imageSaver = ImageSaver()
+                        imageSaver.successHandler = { print("Image saved") }
+                        imageSaver.errorHandler = { print("Oops: \($0.localizedDescription)")}
+                        
+                        imageSaver.writeToPhotoAlbum(image: proccesedImage)
                     }
                 }
             }
@@ -71,22 +117,46 @@ struct ContentView: View {
             .sheet(isPresented: $shownigImagePicker, onDismiss: loadImage) {
                 ImagePicker(image: self.$inputImage)
             }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Select a filter"), buttons: [
+                    .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
+                    .default(Text("Edges")) { self.setFilter(CIFilter.edges()) },
+                    .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur()) },
+                    .default(Text("Pixellate")) { self.setFilter(CIFilter.pixellate()) },
+                    .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone()) },
+                    .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask()) },
+                    .default(Text("Vignette")) { self.setFilter(CIFilter.vignette()) },
+                    .cancel()
+                ])
+            }
+            .alert(isPresented: $showingSavingErrorAlert) {
+                Alert(title: Text("Oh No!"), message: Text("The image couldn't be saved"))
+            }
         }
     }
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        
         let beginImage = CIImage(image: inputImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         applyProccessing()
     }
     func applyProccessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        
         guard let outputImage = currentFilter.outputImage else { return }
+        
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            proccesedImage = uiImage
         }
+    }
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
 }
 
---
+++ Apps/Project 14 - BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Benjamin Keys on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
